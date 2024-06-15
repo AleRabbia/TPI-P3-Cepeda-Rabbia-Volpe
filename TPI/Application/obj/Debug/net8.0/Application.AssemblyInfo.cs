2d477e3b3b56494b2915f5b2fcfc8c97fe2f3fd2
Marcelo <marcelo.cepeda17@gmail.com>
2024-06-15 18:46:01 -0300

    Fixing bugs 3

--- TPI/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ TPI/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e1c04e942ad3d34de19eca55698fc611b89ef5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d52cec560f723335e399ba0fc0f89b42f656b93b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TPI/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ TPI/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e1c04e942ad3d34de19eca55698fc611b89ef5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d52cec560f723335e399ba0fc0f89b42f656b93b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
