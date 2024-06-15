a1e74174e6ddc81448776fea389bfffec7d924c9
MatiVolpe <mati.clc.volpe@gmail.com>
2024-06-15 19:01:06 -0300

    Prueba 3

---
+++ TPI/Presentation/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet ("{Id}")]
+        public ActionResult GetById([FromRoute]Guid id)
+        {
+            return Ok();
+        }
+        
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
