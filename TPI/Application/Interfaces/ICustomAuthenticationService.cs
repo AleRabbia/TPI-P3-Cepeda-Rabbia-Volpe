a2a4d702b596d5790653fcf1f50507c3eed332c1
Marcelo <marcelo.cepeda17@gmail.com>
2024-06-17 11:41:51 -0300

    Adding UserDto

---
+++ TPI/Application/Interfaces/ICustomAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICustomAuthenticationService
+    {
+        string Autenticar(AuthenticationRequest authenticationRequest);
+    }
+}
---
+++ TPI/Application/Models/UserDto.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string CompleteName { get => Name + " " + LastName; }
+        public string Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Role { get; set; }
+
+        public static UserDto Create(User user)
+        {
+            var dto = new UserDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                LastName = user.LastName,
+                Email = user.Email,
+                Birthdate = user.Birthdate,
+                Role = user.Role.ToString()
+            };
+
+            return dto;
+        }
+    }
+}
